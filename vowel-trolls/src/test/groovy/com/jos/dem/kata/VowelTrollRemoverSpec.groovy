5d3104905c8dedc1c7b9ac1a77767c7d9df9ea2b
josdem <joseluis.delacruz@gmail.com>
2021-07-03 17:07:28 -0400

    Adding parameterized test

--- vowel-trolls/src/test/groovy/com/jos/dem/kata/VowelTrollRemoverSpec.groovy
+++ vowel-trolls/src/test/groovy/com/jos/dem/kata/VowelTrollRemoverSpec.groovy
@@ -7,11 +7,14 @@ class VowelTrollRemoverSpec extends Specification {
     private VowelTrollRemover vowelTrollRemover = new VowelTrollRemover();
 
     void "Removing vowels"() {
-        given: "A string"
-        def message = 'This website is for losers LOL!'
         when: "I apply remover"
         def result = vowelTrollRemover.remove(message)
         then: "I verify there is no vowels"
-        result == "Ths wbst s fr lsrs LL!"
+        result == expectedResult
+        where:
+        message                                                             | expectedResult
+        'This website is for losers LOL!'                                   | 'Ths wbst s fr lsrs LL!'
+        "No offense but,\nYour writing is among the worst I've ever read"   | "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+        'What are you, a communist?'                                        | 'Wht r y,  cmmnst?'
     }
 }
