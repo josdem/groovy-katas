a31445e8cee913d6647defbf437272f6e2830563
josdem <joseluis.delacruz@gmail.com>
2021-07-25 09:19:38 -0400

    Adding find the odd kata

---
+++ find-the-odd/src/main/groovy/com/jos/dem/kata/OddFinder.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.kata
+
+/*
+Given an array of integers, find the one that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+ */
+
+class OddFinder {
+
+    def find(def array){
+        return array.size()
+    }
+}
