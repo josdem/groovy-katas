cad1789673a9221760f7c4f932eeb44e1dd99928
josdem <joseluis.delacruz@gmail.com>
2021-08-17 08:27:54 -0400

    Adding most popular kata solution

--- most-popular/src/main/groovy/com/jos/dem/kata/PopularFinder.groovy
+++ most-popular/src/main/groovy/com/jos/dem/kata/PopularFinder.groovy
@@ -8,6 +8,8 @@ Write a function that finds most popular number in the array, in this case 34 be
 class PopularFinder {
 
     def find(def numbers){
-        return numbers.size()
+        def numbersGrouped = numbers.groupBy { numbers.count(it) }
+        def keyMostPopular = numbersGrouped.keySet().max()
+        numbersGrouped.get(keyMostPopular)[0]
     }
 }
