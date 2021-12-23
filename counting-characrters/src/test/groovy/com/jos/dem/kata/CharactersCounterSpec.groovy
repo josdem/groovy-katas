453ddcc521b346ec83798d12728db114daee0df4
josdem <joseluis.delacruz@gmail.com>
2021-12-23 16:37:37 -0600

    Refactoring test case making static keyword

--- counting-characrters/src/test/groovy/com/jos/dem/kata/CharactersCounterSpec.groovy
+++ counting-characrters/src/test/groovy/com/jos/dem/kata/CharactersCounterSpec.groovy
@@ -4,17 +4,18 @@ import spock.lang.Specification
 
 class CharactersCounterSpec extends Specification {
 
+    private static final def KEYWORD = "josdem"
     private def charactersCounter = new CharactersCounter()
 
     void "should count vowels"() {
         when: "I call vowels counter"
-        def result = charactersCounter.countVowels("josdem")
+        def result = charactersCounter.countVowels(KEYWORD)
         then: "I verify vowels quantity"
         result == 2
     }
 
-    void "should countconsonants"() { when: "I call consonants counter"
-        def result = charactersCounter.countConsonants("josdem")
+    void "should count consonants"() { when: "I call consonants counter"
+        def result = charactersCounter.countConsonants(KEYWORD)
         then: "I verify consonants quantity"
         result == 4
     }
