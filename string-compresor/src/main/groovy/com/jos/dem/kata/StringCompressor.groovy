1d33dc935f2aff17a3c4a6045e134ceac11715f4
josdem <joseluis.delacruz@gmail.com>
2021-12-29 13:24:40 -0600

    Adding string compressor kata solution

--- string-compresor/src/main/groovy/com/jos/dem/kata/StringCompressor.groovy
+++ string-compresor/src/main/groovy/com/jos/dem/kata/StringCompressor.groovy
@@ -8,6 +8,10 @@ Then we have a compressed string like "a3b4c2"
 
 class StringCompressor {
     def compress(String keyword) {
-        keyword
+        def result = new StringBuilder()
+        def array = keyword.split('')
+        def map = array.groupBy {array.count (it)}
+        map.each {result << it.value[0] << it.key}
+        result.toString()
     }
 }
