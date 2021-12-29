2e26713be3ee9d752a16a54b77e73fadb858d130
josdem <joseluis.delacruz@gmail.com>
2021-12-28 19:08:55 -0600

    Adding string compressor kata

---
+++ string-compresor/src/main/groovy/com/jos/dem/kata/StringCompressor.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.kata
+
+/*
+Given a string "aaabbbbcc"
+When we call compress method
+Then we have a compressed string like "a3b4c2"
+ */
+
+class StringCompressor {
+    def compress(String keyword) {
+        keyword
+    }
+}
---
+++ string-compresor/src/test/groovy/com/jos/dem/kata/StringCompressorSpec.groovy
@@ -0,0 +1,19 @@
+package com.jos.dem.kata
+
+import spock.lang.Specification
+
+class StringCompressorSpec extends Specification {
+
+    def stringCompressor = new StringCompressor()
+
+    void "should compress a string"(){
+        when: "I call to compress method"
+        def result = stringCompressor.compress(keyword)
+        then: "I verify is a compressed string"
+        result == expectedResult
+        where:
+        keyword     |   expectedResult
+        "aaabbbbcc" |   "a3b4c2"
+
+    }
+}
