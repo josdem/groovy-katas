5a9cf79b7bf452ab2fe8dd6476aeedd5b6e11020
josdem <joseluis.delacruz@gmail.com>
2022-01-04 20:41:04 -0600

    Adding smallest biggest kata

---
+++ smallest-biggest/src/main/groovy/com/jos/dem/kata/SmallestBiggestFinder.groovy
@@ -0,0 +1,15 @@
+package com.jos.dem.kata
+
+/*
+Find smaller and biggest numbers in a collection
+Given: A collection like [7, 5, 2, 4, 3, 9]
+When: I call find method
+Then: I will get a collection with [2, 9] values
+ */
+
+class SmallestBiggestFinder {
+
+    def find(numbers){
+        numbers
+    }
+}
