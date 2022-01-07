1bdd8f35ba3f261ae078eb99044a2531e0619bc4
josdem <joseluis.delacruz@gmail.com>
2022-01-07 15:23:39 -0600

    Adding kata definition

--- electronic-shop/src/test/groovy/com/jos/dem/kata/ShopCalculatorSpec.groovy
+++ electronic-shop/src/test/groovy/com/jos/dem/kata/ShopCalculatorSpec.groovy
@@ -1,5 +1,16 @@
 package com.jos.dem.kata
 
+/*
+Monica wants to buy exactly one keyboard and one USB drive from her favorite electronics store.
+The store sells N different brands of keyboards and M different brands of USB drives.
+Monica has exactly S dollars to spend, and she wants to spend as much of it as possible (i.e., the total cost of her purchase must be maximal).
+Given: Amount of money Monica has to spend in electronics as 10
+And: Keyboards prices as [3, 1]
+And: Usbs' prices as [5, 2, 8]
+When: I call to compute method
+Then: I will have 9 as result
+ */
+
 import spock.lang.Specification
 
 class ShopCalculatorSpec extends Specification {
